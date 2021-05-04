24311855fa99ef769358f7b7001fd2405ab022bb
AimeeJay <ajesso@post.com>
2021-05-04 16:38:31 -0400

    unique id npm install and start of post request

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const path = require("path");
 const express = require("express");
 //Enable and use Express for GET, POST and (DELETE*bonus)
 const app = express();
-//Enabling use of other files like CSS, JS and middleware (parsing user data the right was)
+//Enable unique ID npm package (UUID)
+const { v4: uuidv4 } = require('uuid');
+//Enabling use of other files like CSS, JS and middleware (parsing user data the right way)
 app.use(express.static('public'));
 //Enable ability to retrieve saved user notes
 app.use(express.json());
@@ -25,3 +27,18 @@ app.get("*", (req,res) => {
 app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname,"/public/notes.html"))
 });
+
+//The following API routes should be created:
+//GET /api/notes
+app.get("/api/notes", (req,res) => {
+    res.sendFile(path.join(__dirname,"/db/db.json"))
+});
+
+//POST /api/notes  (when new notes are created)
+//should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+app.post("", (req,res) => {
+    let noteId = uuid.stringify();
+    let userTitle = req.body;
+    let userNote = req.body;
+    let noteLi = JSON.parse
+})
