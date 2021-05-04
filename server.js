b2724551d7fd089dcad46e35934c9a136040241a
AimeeJay <ajesso@post.com>
2021-05-04 14:56:59 -0400

    added some require statements and routes

---
+++ server.js
@@ -0,0 +1,25 @@
+//Enable and use Express for GET, POST and (DELETE*bonus)
+const app = express();
+//Enable file system for use
+const fs = require("fs");
+//Enable use of file and directory paths
+const path = require("path");
+//Enabling use of other files like CSS, JS and middleware (parsing user data the right was)
+app.use(express.static('public'));
+//Enable ability to retrieve saved user notes
+app.use(express.json());
+//Using express to thoroughly and properly use/"unpack" JSON objects
+app.use(express.urlencoded({ extended: true }));
+//Setting a port variable to be used in multple areas of js files
+const PORT = process.env.PORT || 3001;
+
+
+//The following HTML routes should be created
+//get every note to index html
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"/public/index.html"))
+});
+//get to notes html
+app.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname,"/public/notes.html"))
+});
