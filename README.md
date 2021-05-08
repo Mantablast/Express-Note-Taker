9fb7c4486c8e24c7c5d0d1c97df668d9aff4fda1
AimeeJay <ajesso@post.com>
2021-05-07 22:24:42 -0400

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Express Note Taker
+# Express Note Taker ©2021 
+
+## Description
+Express Note Taker is a tool made to organize, update and deleted tasks in the browser.  ![image](./public/assets/images/NoteTakerScreen.png)
+
+Project by: Aimee J  
+Main Github: [MissAimeeJay](https://github.com/MissAimeeJay)
+Repository Link: https://github.com/MissAimeeJay/Express-Note-Taker  
+Live Page Url:  
+[https://express-note-takr.herokuapp.com/](https://express-note-takr.herokuapp.com/)
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Testing](#testing)
+* [Questions](#questions)
+
+## Installation
+The project should not require installing any dependencies as a heroku link has been provided to use.
+
+## Usage 
+This web application can be used to schedule any task.  Once the task has been completed, it can be deleted from the task list side bar.
+
+## License
+[Click here for more information on this license](https://choosealicense.com/licenses/unlicense)
+
+
+## Testing
+Note that the tasks entered will not enter the side bar until the save icon has been clicked.  The save icon will only appear when text has been entered into the fields.
+
+## Questions
+If you have questions or would like to contact for other reasons, please contact
+Aimee J  
+Email: ajesso@post.com
+Main Github: [MissAimeeJay](https://github.com/MissAimeeJay)
--- server.js
+++ server.js
@@ -22,27 +22,27 @@ let noteLi = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 //get every note to index html
 
 //get to notes html
-app.get("/notes", (req,res) => {
-    res.sendFile(path.join(__dirname,"/public/notes.html"))
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
 //The following API routes should be created:
 //GET /api/notes
-app.get("/api/notes", (req,res) => {
-    res.sendFile(path.join(__dirname,"/db/db.json"))
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
 });
 
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname,"/public/index.html"))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 //POST /api/notes  (when new notes are created)
 //should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-app.post("/api/notes", (req,res) => {
+app.post("/api/notes", (req, res) => {
     const noteId = uuidv4();
     // let userTitle = req.title;
     let wholeNote = req.body;
     wholeNote.id = noteId;
-    
+
     //Push the items to a single array
     noteLi.push(wholeNote);
     //Write parsed note to json database
@@ -51,21 +51,21 @@ app.post("/api/notes", (req,res) => {
     console.log(noteLi + "wrote to database");
 });
 
-app.delete('/api/notes/:id', (req,res) => {
-//Make a condition that if id matches id in json object that was clicked, filter out and update li to json
-//Assign jason object to a variable access in db.json
-const { id } = req.params;
-//gather id from requested param and find it in the noteLi
-const findTheNote = noteLi.find(noteLi => noteLi.id === id);
-console.log ("The requested id:" + id);
-console.log ("After searching note list: " + findTheNote);
-//if it is present
-if(findTheNote) {
-    //filter out all items that do NOT match id
-    noteLi = noteLi.filter(noteLi => noteLi.id !== id);
-} else {
-    res.status(404).json({message: "Note item was not found."})
-}
+app.delete('/api/notes/:id', (req, res) => {
+    //Make a condition that if id matches id in json object that was clicked, filter out and update li to json
+    //Assign jason object to a variable access in db.json
+    const { id } = req.params;
+    //gather id from requested param and find it in the noteLi
+    const findTheNote = noteLi.find(noteLi => noteLi.id === id);
+    console.log("The requested id:" + id);
+    console.log("After searching note list: " + findTheNote);
+    //if it is present
+    if (findTheNote) {
+        //filter out all items that do NOT match id
+        noteLi = noteLi.filter(noteLi => noteLi.id !== id);
+    } else {
+        res.status(404).json({ message: "Note item was not found." })
+    }
     //write new noteLi to json
     fs.writeFileSync("./db/db.json", JSON.stringify(noteLi));
     res.json(noteLi);
